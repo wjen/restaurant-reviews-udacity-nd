fdbf4aaa95af64fb27062b07664602bd3a1b82a6
Wen Jen <wjen85@gmail.com>
2018-07-01 20:43:19 -0700

    readjust vertical alignment with new max-width for media queries

--- js/main.js
+++ js/main.js
@@ -163,22 +163,26 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const textArea = document.createElement('div')
+  textArea.classList.add('textArea');
+  li.append(textArea);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  textArea.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  textArea.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  textArea.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  textArea.append(more)
 
   return li
 }
