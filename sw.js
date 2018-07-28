be81f038d55b66610ac228e70047f26a1f6429a7
Wen Jen <wjen85@gmail.com>
2018-07-27 19:13:39 -0700

    setup service worker cache install

--- sw.js
+++ sw.js
@@ -1,2 +1,27 @@
+let cacheID = "mws-restaurant-01";
+let urlsToCache = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/js',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/js/sw/register.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheID).then(cache => {
+      return cache
+        .addAll(urlsToCache)
+    }).catch(error => {
+        console.log("Caches open failed " + error);
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {console.log(event.request);
 });
